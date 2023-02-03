3047194053aae9a977630661bfbc7a6d93024e05
adrianaa27 <adriana@northstar-tek.com>
2023-02-03 08:39:26 -0600

    testing

--- testing.py
+++ testing.py
@@ -1,19 +1,11 @@
 from napalm import get_network_driver
 from rich import print as rprint
 import json 
+import csv
+import yaml
+from jinja2 import Environment, FileSystemLoader
 
-#---
-# #
-junos_driver = get_network_driver("junos")
-ex4300 = junos_driver('10.100.0.8', 'test', 'H@ppyrout3')
-ex2200 = junos_driver('10.196.29.170', 'root', 'H@ppyrout3')
-
-devices = [ex4300, ex2200]
-
-for device in devices:
-    device.open()
-    output = device.get_lldp_neighbors_detail()
-    print(json.dumps(output, sort_keys=True, indent=4))
-
-#---#
-
+with open('getfacts.csv','w') as getfacts_data:
+   
+    for facts in getfacts_data:
+        getfacts_data.write({napalm_facts{fqdn[0]}}+",\n")
