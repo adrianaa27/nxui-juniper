4d2d9fcc0c52e9eae80ccb867a540d73103eeb80
adrianaa27 <adriana@northstar-tek.com>
2023-02-03 10:27:22 -0600

    print row

--- testing.py
+++ testing.py
@@ -8,7 +8,7 @@ from jinja2 import Environment, FileSystemLoader
 with open('getfacts.csv','r+') as getfacts_data:
     reader = csv.reader(getfacts_data)
     for row in reader:
-        #rprint(row)
-        getfacts_data.write(row[0])
+        print(row + "\n")
+        #getfacts_data.write(row[0])
 
 
