20e85eaa1a925a40e97098e4a89ff3aa0f9976d3
adrianaa27 <adriana@northstar-tek.com>
2023-02-03 08:45:04 -0600

    napalm facts

--- testing.py
+++ testing.py
@@ -8,4 +8,4 @@ from jinja2 import Environment, FileSystemLoader
 with open('getfacts.csv','w') as getfacts_data:
    
     for facts in getfacts_data:
-        getfacts_data.write({napalm_facts{fqdn[0]}}+",\n")
+        getfacts_data.write(napalm_fqdn[0]+",\n")
