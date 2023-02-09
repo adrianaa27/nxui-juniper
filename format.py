3987c7d3bc268ce1ef981a79be38f4ea9adceda5
adrianaa27 <adriana@northstar-tek.com>
2023-02-09 10:49:24 -0600

    test

--- format.py
+++ format.py
@@ -6,13 +6,13 @@ from jinja2 import Environment, FileSystemLoader
 with open('getfacts.csv', 'r') as csv_data:
     csv_reader = csv.reader(csv_data)
     #print(csv_reader)
-    with open('format.yaml','w') as destination_data:
+    with open('format.yml','w') as destination_data:
         destination_data.write("---\ndevice:" + "\n")
     for line in csv_reader:
         
         #open up the context manager to the file destination.txt
     
-        with open('format.yaml','a') as destination_data:
+        with open('format.yml','a') as destination_data:
             #destination_data.write("device:" + "\n    ")
             destination_data.write("  - device_name: "+'"'+ line[0]+'"'+"\n    ")
             destination_data.write("host_ip: " + line[1] + "\n")
