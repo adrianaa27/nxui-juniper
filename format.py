8490a14323a55916e921dc81c9adcb4e7c72e2f3
adrianaa27 <adriana@northstar-tek.com>
2023-02-09 11:14:11 -0600

    t

--- format.py
+++ format.py
@@ -14,7 +14,7 @@ with open('getfacts.csv', 'r') as csv_data:
     
         with open('format.yml','a') as destination_data:
             #destination_data.write("device:" + "\n    ")
-            destination_data.write("  - device_name: "+'"'+ line[0]+'"'+"\n    ")
-            destination_data.write("        host_ip: " + line[1] + "\n")
-            destination_data.write("        details: " + line[2] + "\n")
+            destination_data.write("- device_name: "+'"'+ line[0]+'"'+"\n    ")
+            destination_data.write("    host_ip: " + line[1] + "\n")
+            destination_data.write("    details: " + line[2] + "\n")
 
