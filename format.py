bddbc97062db5940e369b51182cd43e1e837c82c
adrianaa27 <adriana@northstar-tek.com>
2023-02-09 17:35:35 -0600

    t

--- format.py
+++ format.py
@@ -21,7 +21,7 @@ with open('getfacts.csv', 'r') as csv_data:
 
 with open('lldpfacts.txt', 'r') as lldp_file:
     txt_reader = lldp_file.readlines()
-    print(txt_reader[0]) 
+    print(txt_reader[1]) 
     
        
         
