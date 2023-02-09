dbbb636b7f1824232c927daef960372e0484c7da
adrianaa27 <adriana@northstar-tek.com>
2023-02-09 11:26:21 -0600

    t

--- format.py
+++ format.py
@@ -9,13 +9,13 @@ with open('getfacts.csv', 'r') as csv_data:
     with open('format.yml','w') as destination_data:
         destination_data.write("---\ndevice:" + "\n")
         for line in csv_reader:
-        #open up the context manager to the file destination.txt
+            #open up the context manager to the file destination.txt
     
-        with open('format.yml','a') as destination_data:
+            with open('format.yml','a') as destination_data:
             #destination_data.write("device:" + "\n    ")
-            destination_data.write("- device_name: "+'"'+ line[0]+'"'+"\n")
-            destination_data.write("    host_ip: " + line[1] + "\n")
-            destination_data.write("    details: " + line[2] + "\n")
+                destination_data.write("- device_name: "+'"'+ line[0]+'"'+"\n")
+                destination_data.write("    host_ip: " + line[1] + "\n")
+                destination_data.write("    details: " + line[2] + "\n")
             
 
 
