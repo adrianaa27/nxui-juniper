d56e244d605b9c4752fbd1419c5da7975f417784
adrianaa27 <adriana@northstar-tek.com>
2023-02-10 14:02:09 -0600

    t

--- format.py
+++ format.py
@@ -11,7 +11,7 @@ with open('getfacts.csv', 'r') as csv_data:
         for line in csv_reader:
             #open up the context manager to the file destination.txt
             with open('format.yml','a') as destination_data:
-                destination_data.write(" - dev_name:" + line[0]+ "\n")
+                destination_data.write("\n\n\n - dev_name:" + line[0]+ "\n")
                 destination_data.write("host_ip: " + line[1] + "\n")
                 #destination_data.write("    details: " + line[2] + "\n")
         
