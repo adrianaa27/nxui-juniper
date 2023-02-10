b795d078c38272f5bb3eca9d82e27687acc494a1
adrianaa27 <adriana@northstar-tek.com>
2023-02-10 10:37:24 -0600

    t

--- format.py
+++ format.py
@@ -18,8 +18,6 @@ with open('getfacts.csv', 'r') as csv_data:
                 #destination_data.write("    details: " + line[2] + "\n")
         
 
-
-
 lldp_dl = {}
 with open('lldpfacts.txt', 'r') as lldp_file:
     txt_reader = lldp_file.readlines()
@@ -41,13 +39,10 @@ with open('lldpfacts.txt', 'r') as lldp_file:
                 destination_data.write("    remote_port_description: "+ i['remote_port_description'] +"\n")   
                 destination_data.write("    remote_system_name: "+ i['remote_system_name'] +"\n")
                 destination_data.write("    remote_system_description: "+ i['remote_system_description'] +"\n")   
-                    #destination_data.write("    remote_system_capab: "+ i['remote_system_capab'] +"\n")   
-                    #destination_data.write("    remote_system_enable_capab: "+ i['remote_system_enable_capab'] +"\n")  
+                destination_data.write("    remote_system_capab: "+ i['remote_system_capab'][0] +"\n")   
+                destination_data.write("    remote_system_enable_capab: "+ i['remote_system_enable_capab'][0] +"\n")  
                 destination_data.write("    parent_interface: "+ i['parent_interface'] +"\n")   
- 
-
-
-                    #print(i['remote_system_capab'])
+           
                 
 
 
