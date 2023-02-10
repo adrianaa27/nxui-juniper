2379f9ba159d5e6ccceddb3a915676c4bdbd1115
adrianaa27 <adriana@northstar-tek.com>
2023-02-10 08:51:39 -0600

    t

--- format.py
+++ format.py
@@ -29,7 +29,7 @@ with open('lldpfacts.txt', 'r') as lldp_file:
 for k, v in lldp_dl.items():
     for i in v:
         #print(k, ":", i)
-        for key, values  in i:
+        for key, values  in i.items():
             print(k, ":", values)
 
 
