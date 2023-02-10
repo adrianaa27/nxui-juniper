497db3eaab7e1dad98634169838d648c281dddf4
adrianaa27 <adriana@northstar-tek.com>
2023-02-10 09:54:16 -0600

    t

--- format.py
+++ format.py
@@ -33,7 +33,7 @@ with open('lldpfacts.txt', 'r') as lldp_file:
             #with open('format.yml','a') as destination_data:
                 #destination_data.write("    details: "+ port +"\n")        
             for items in i:
-                print(i[items])
+                print(i['remote_chassis_id'])
                 
 
 
