1fb3155219e8bd41df7c3eeb16947ed092ee95df
adrianaa27 <adriana@northstar-tek.com>
2023-02-14 15:07:47 -0600

    t

--- netBuild.py
+++ netBuild.py
@@ -4,17 +4,17 @@ import os
 from napalm import get_network_driver
 
 junos_driver = get_network_driver('junos')
-ios_driver = get_network_driver('ios')
+#ios_driver = get_network_driver('ios')
 
 #ex4300 = junos_driver('10.100.0.8', 'teset', 'H@ppyrout3')
 #devices = [ex4300]
 
 with junos_driver('10.100.0.8', 'test', 'H@ppyrout3') as device:
-    #print(device.get_facts())
-    #print(device.get_lldp_neighbors_detail())
-    lldp_data = {}
-    lldp_data = (device.get_lldp_neighbors)
-    device_facts = {}
-    print(lldp_data)
+    result = [device.get_facts(),device.get_lldp_neighbors_detail()]
+    print(result)
+    #lldp_data = {}
+    #lldp_data = (device.get_lldp_neighbors)
+    #device_facts = {}
+    #print(lldp_data)
 
 #for dev, output in device.get_facts().items:
