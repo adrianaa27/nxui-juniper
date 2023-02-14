e634a7978fb24a41c2abe69a4a08440b2f78d9ed
adrianaa27 <adriana@northstar-tek.com>
2023-02-14 09:19:02 -0600

    format.yml

--- format.py
+++ format.py
@@ -11,8 +11,8 @@ with open('getfacts.csv', 'r') as csv_data:
     for line in csv_reader:
     #open up the context manager to the file destination.txt
         with open('format.yml','a') as destination_data:
-            destination_data.write("  - device_name: " + line[0]+ "\n")
-            destination_data.write("  host_ip: " + line[1] + "\n")
+            destination_data.write("    - device_name: " + line[0]+ "\n")
+            destination_data.write("    host_ip: " + line[1] + "\n")
                 #destination_data.write("    details: " + line[2] + "\n")
 
 lldp_dl = {}
@@ -25,15 +25,15 @@ with open('lldpfacts.txt', 'r') as lldp_file:
         for i in port_details:
             #print(port, ":", i)  
              with open('format.yml','a') as destination_data:
-                destination_data.write("    - port: "+ port +"\n")   
-                destination_data.write("        remote_chassis_id: "+ i['remote_chassis_id'] +"\n")
-                destination_data.write("        remote_port: "+ i['remote_port'] +"\n") 
-                destination_data.write("        remote_port_description: "+ i['remote_port_description'] +"\n")   
-                destination_data.write("        remote_system_name: "+ i['remote_system_name'] +"\n")
-                destination_data.write("        remote_system_description: "+ i['remote_system_description'] +"\n")   
-                destination_data.write("        remote_system_capab: "+ i['remote_system_capab'][0] +"\n")   
-                destination_data.write("        remote_system_enable_capab: "+ i['remote_system_enable_capab'][0] +"\n")  
-                destination_data.write("        parent_interface: "+ i['parent_interface'] +"\n")   
+                destination_data.write("        - port: "+ port +"\n")   
+                destination_data.write("            remote_chassis_id: "+ i['remote_chassis_id'] +"\n")
+                destination_data.write("            remote_port: "+ i['remote_port'] +"\n") 
+                destination_data.write("            remote_port_description: "+ i['remote_port_description'] +"\n")   
+                destination_data.write("            remote_system_name: "+ i['remote_system_name'] +"\n")
+                destination_data.write("            remote_system_description: "+ i['remote_system_description'] +"\n")   
+                destination_data.write("            remote_system_capab: "+ i['remote_system_capab'][0] +"\n")   
+                destination_data.write("            remote_system_enable_capab: "+ i['remote_system_enable_capab'][0] +"\n")  
+                destination_data.write("            parent_interface: "+ i['parent_interface'] +"\n")   
 
 
 def generate_hosts():
