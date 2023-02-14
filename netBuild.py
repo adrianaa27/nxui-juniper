e2fd405c60753a3ffdb7121195b5d638ff1cd1fe
adrianaa27 <adriana@northstar-tek.com>
2023-02-14 15:58:20 -0600

    s

--- netBuild.py
+++ netBuild.py
@@ -12,9 +12,7 @@ junos_driver = get_network_driver('junos')
 with junos_driver('10.100.0.8', 'test', 'H@ppyrout3') as device:
     lldp_result = [device.get_lldp_neighbors_detail()]
     facts_result = [device.get_facts()]
+    
+    lldp_details = lldp_result[1]
     print(lldp_result)
-    for items in lldp_result:
-        print(items)
-   # for interface, int_details in lldp_result.items():
-    #    for i in int_details:
-     #       print(i + ":" + int_details)
+  
