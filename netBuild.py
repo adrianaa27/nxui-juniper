6d9e3c6307323919fba3a2d9f57d39d3f73304be
adrianaa27 <adriana@northstar-tek.com>
2023-02-14 13:35:44 -0600

    test

--- netBuild.py
+++ netBuild.py
@@ -1,3 +1,6 @@
+import napalm
+import sys
+import os
 from napalm import get_network_driver
 from napalm import 
 
@@ -5,11 +8,10 @@ from napalm import
 ios_driver = get_network_driver('junos')
 junos_driver = get_network_driver('ios')
 
-ex4300 = junos_driver('10.100.0.8', 'teset', 'H@ppyrout3')
+#ex4300 = junos_driver('10.100.0.8', 'teset', 'H@ppyrout3')
+#devices = [ex4300]
 
-devices = [ex4300]
-
-for device in devices:
-    device.open()
-    output = device.get_facts(), device.get_lldp_neighbors_detail()
-    print(output)
+with junos_driver('10.100.0.8', 'teset', 'H@ppyrout3') as device:
+    print(device.get_facts())
+    print(device.get_lldp_neighbors_detail())
+    
