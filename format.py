acb199523cb36e80761039a865684c37fdf13fe0
adrianaa27 <adriana@northstar-tek.com>
2023-02-14 09:46:55 -0600

    t

--- format.py
+++ format.py
@@ -7,7 +7,7 @@ with open('getfacts.csv', 'r') as csv_data:
     csv_reader = csv.reader(csv_data)
     #print(csv_reader)
     with open('format.yml','w') as destination_data:
-        destination_data.write("---\ndevice: \n")
+        destination_data.write("---\ndevice:\n")
     for line in csv_reader:
     #open up the context manager to the file destination.txt
         with open('format.yml','a') as destination_data:
