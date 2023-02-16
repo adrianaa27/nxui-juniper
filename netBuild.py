1ea9f69729e599f32fd9af3150f7a3943699eb74
adrianaa27 <adriana@northstar-tek.com>
2023-02-16 09:09:38 -0600

    t

--- netBuild.py
+++ netBuild.py
@@ -12,7 +12,7 @@ junos_driver = get_network_driver('junos')
 with junos_driver('10.100.0.8', 'test', 'H@ppyrout3') as device:
     #lldp_result = device.get_lldp_neighbors_detail()
     #facts_result = device.get_facts()
-    result = {device.get_facts(),device.get_bgp_neighbors_detail()}
+    result = device.get_facts(),device.get_bgp_neighbors_detail()
     #print(lldp_result)
     print(result)
     #for port, port_details in lldp_result.items():
