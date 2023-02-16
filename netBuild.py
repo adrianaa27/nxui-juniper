12de06f35ffae82984bc4cc03c64f3954d197b35
adrianaa27 <adriana@northstar-tek.com>
2023-02-16 10:47:46 -0600

    t

--- netBuild.py
+++ netBuild.py
@@ -6,24 +6,42 @@ from napalm import get_network_driver
 junos_driver = get_network_driver('junos')
 #ios_driver = get_network_driver('ios')
 
-#ex4300 = junos_driver('10.100.0.8', 'teset', 'H@ppyrout3')
-#devices = [ex4300]
-
+#devices = [ex4300, ]
+#network_devices = []
+    #for device in devices:
+        #if device[1] == "ios":
+            #network_devices.append(
+                            #driver_ios(
+                            #hostname = device[0],
+                            #username = "admin",
+                            #password = "Time4work!"
+                            #)
+                              #)
+        #elif device[1] == "junos":
+            #network_devices.append(
+                            #driver_junos(
+                            #hostname = device[0],
+                            #username = "test",
+                            #password = "H@ppyrout3"
+                            #)
+                              #)
+#for device in network_devices:
+        #device.open()
 with junos_driver('10.100.0.8', 'test', 'H@ppyrout3') as device:
     lldp_result = device.get_lldp_neighbors_detail()
     facts_result = device.get_facts()
     #print(lldp_result)
+
     print(facts_result)
-    #for port, port_details in lldp_result.items():
-        #for i in port_details:
-            #print(port, ":" ,i)
     lldp_data = {}
     facts = {}
-
     for device, fact_output in facts_result.items():
         facts['ip'] = facts_result['fqdn']
         facts['role'] = facts_result['model']
-    print(device)
+    #print(facts)
+        for port, port_details in lldp_result.items():
+            lldp_data[facts]['ip'] = lldp_result 
+            print (lldp_data)
         
 
 
